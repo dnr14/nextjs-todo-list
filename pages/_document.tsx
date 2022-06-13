eec80f861ba55af37ca9094a1e9238e29c2d927d
dnr14 <dnr14@naver.com>
2022-06-13 23:46:08 +0900

    chore : google font 추가

---
+++ pages/_document.tsx
@@ -0,0 +1,25 @@
+import { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
