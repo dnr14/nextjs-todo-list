92ed17d9b81ddf0cb4f36ce71606c045945c6c69
dnr14 <dnr14@naver.com>
2022-06-15 22:23:20 +0900

    feat : NotFound 컴포넌트 생성

---
+++ pages/404.tsx
@@ -0,0 +1,32 @@
+import { Button } from '@components/Button'
+import { Text } from '@components/Text'
+import Link from 'next/link'
+import React from 'react'
+import { styled } from '../stitches.config'
+
+
+const NotFoundWrapper = styled('div',{
+    display:'flex',
+    justifyContent:"center",
+    alignItems:"center",
+    padding:'20px 0',
+    flexDirection:'column'
+})
+
+
+export default function notFount() {
+  
+    return (
+    <NotFoundWrapper>
+        <Text>
+            없는 TODO 페이지입니다.
+        </Text>
+        <Button>
+            <Link href={'/'}>
+            돌아가기
+            </Link>
+        </Button>
+    </NotFoundWrapper>
+  )
+}
+
