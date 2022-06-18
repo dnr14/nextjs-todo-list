8dc660f7c41cf2658153d0afaf807c31fd3a1a39
dnr14 <dnr14@naver.com>
2022-06-18 22:56:39 +0900

    fix : Text컴포넌트 수정
    props margin 옵셔널로 추가

--- components/Text/index.tsx
+++ components/Text/index.tsx
@@ -1,9 +1,11 @@
+import { css } from "@stitches/react";
 import React, { FC } from "react";
 
 interface TextProps {
   children: React.ReactNode;
+  margin?: string;
 }
 
-export const Text: FC<TextProps> = ({ children }) => {
-  return <p>{children}</p>;
-};
+export const Text: FC<TextProps> = ({ children, margin }) => (
+  <p className={css({ margin })()}>{children}</p>
+);
