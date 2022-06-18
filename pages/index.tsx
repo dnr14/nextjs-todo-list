3983a9ad9327bf872be43a88187714d0ed1a6264
dnr14 <dnr14@naver.com>
2022-06-18 22:58:16 +0900

    feat : index페이지 기능 추가
    todos pre-fetch 해오는 기능 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,64 @@
 import type { NextPage } from "next";
-import { theme } from "../stitches.config";
-import { Title, Text, Input } from "@components/index";
+import { Title, Text } from "@components/index";
+import React, { useEffect, useState } from "react";
+import { styled } from "@stitches/react";
 
-const Home: NextPage = () => {
+interface Todo {
+  id: number;
+  content: string;
+}
+
+interface HomeProps {
+  todos: Todo[];
+}
+
+function timestamp() {
+  const today = new Date();
+  today.setHours(today.getHours() + 9);
+  return today.toISOString().replace("T", " ").substring(0, 19);
+}
+
+const Home: NextPage<HomeProps> = ({ todos }) => {
+  const [time, setTime] = useState<string>(timestamp());
+
+  useEffect(() => {
+    const timer = setInterval(() => setTime(timestamp()), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <div className={theme}>
+    <TodoContainer>
+      <Title text={`현재 시간 : ${time}`} />
       <Title text="오늘의 할 일" />
-      {Array.from({ length: 5 }, (_, idx) => (
-        <Text key={idx}>{`넥스트js공부하기 ${idx + 1}`}</Text>
+      {todos.map(({ content, id }, idx) => (
+        <TodoWrapper key={id}>
+          {idx + 1}. <Text margin={"0"}>{content}</Text>
+        </TodoWrapper>
       ))}
-      <Text>json mock serve 뛰우기</Text>
-      <Text>스티치 공부하기</Text>
-    </div>
+    </TodoContainer>
   );
+};
+
+const TodoContainer = styled("section", {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const TodoWrapper = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  gap: 10,
+  margin: "5px 0",
+});
+
+export const getStaticProps = async () => {
+  // fetch 실패 시 에러 처리를 해야된다.
+  const res = await fetch("http://localhost:4300/todos");
+  const todos = await res.json();
+
+  return { props: { todos } };
 };
 
 export default Home;
