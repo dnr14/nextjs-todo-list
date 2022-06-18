e13695148862c51d10db8cc0fef6d9d76ca64c48
dnr14 <dnr14@naver.com>
2022-06-18 22:56:53 +0900

    feat : 404페이지 추가

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,32 +1,24 @@
-import { Button } from '@components/Button'
-import { Text } from '@components/Text'
-import Link from 'next/link'
-import React from 'react'
-import { styled } from '../stitches.config'
+import { Button } from "@components/Button";
+import { Text } from "@components/Text";
+import Link from "next/link";
+import React from "react";
+import { styled } from "../stitches.config";
 
-
-const NotFoundWrapper = styled('div',{
-    display:'flex',
-    justifyContent:"center",
-    alignItems:"center",
-    padding:'20px 0',
-    flexDirection:'column'
-})
-
+const NotFoundWrapper = styled("div", {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "20px 0",
+  flexDirection: "column",
+});
 
 export default function notFount() {
-  
-    return (
+  return (
     <NotFoundWrapper>
-        <Text>
-            없는 TODO 페이지입니다.
-        </Text>
-        <Button>
-            <Link href={'/'}>
-            돌아가기
-            </Link>
-        </Button>
+      <Text>없는 TODO 페이지입니다.</Text>
+      <Button>
+        <Link href={"/"}>돌아가기</Link>
+      </Button>
     </NotFoundWrapper>
-  )
+  );
 }
-
