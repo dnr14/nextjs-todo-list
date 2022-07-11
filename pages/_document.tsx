cf785c04fe04ef0ca3591d2f4a777b9498bd88c1
dnr14 <dnr14@naver.com>
2022-07-11 23:43:46 +0900

    feat : title컴포넌트 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap"
           rel="stylesheet"
         />
+        <title>Todo List</title>
       </Head>
       <body>
         <Main />
